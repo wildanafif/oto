d552065c7877c92c41a7fb62ccc7969bb2f36953
wildan afif <wildan afif>
2016-03-18 14:13:29 -0700

    pervbaikan head view

--- application/views/head.php
+++ application/views/head.php
@@ -7,7 +7,7 @@
     <meta name="description" content="Otomotifstore tempat jual beli otomotif"/>
     <meta name="keywords" content="otomotifstore <?php if (isset($meta['keywords'])) {
         echo ",".$meta['keywords'];
-    } ?>" />\
+    } ?>" />
     <link href="<?=base_url()?>assets/css/social-sharing-buttons.css" rel="stylesheet" type="text/css" media="all" />   
     <link href="<?=base_url()?>assets/css/style_image_view.css" rel="stylesheet" type="text/css" media="all" />   
     <link href="<?=base_url()?>assets/css/Site.min.css" rel="stylesheet">
